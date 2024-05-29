844f4cb8a14c499503c94ab42f621a55420c0ab3
sowmiya-Datascience <85795111+sowmiya-Datascience@users.noreply.github.com>
2024-05-29 22:49:16 +0530

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,9 @@ if ingredients_list:
     ingredients_string = ''
     for furit_chosen in ingredients_list:
         ingredients_string += furit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -40,8 +43,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered!,{name_on_order}',icon="✅")
-#New section to display fruityvice nutrition information
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
